93cf92f4ff55af12d73b6fcd9d7034502645894b
bbmlino-prog <bbmlino@gmail.com>
2025-12-15 10:59:08 -0100

    Update App.py

--- App.py
+++ App.py
@@ -1,9 +1,10 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 st.header("Introduzindo os elementos do Streamlit") 
 
 menu = option_menu(menu_title="Menu",
-                   options=["inicio", "Gráficos Estatísticos", "Gráfico Dinámicos", "Widgets", "Formulários",]
+                   options=["início", "Gráficos Estatísticos", "Gráfico Dinámicos", "Widgets", "Formulários",]
                    Icons=["house", "bar-chart", "bar-chart-line", "toggles", "bar-chart",]
                    Menu_icon="cast",
                    defaut_index=o,
