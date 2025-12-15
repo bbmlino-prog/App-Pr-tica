b0ed144563aae555244e25c7b4f22ce3751f7965
bbmlino-prog <bbmlino@gmail.com>
2025-12-15 11:46:16 -0100

    Update App.py

--- App.py
+++ App.py
@@ -18,7 +18,7 @@ menu = option_menu(menu_title="Menu",
 with st.sidebar:
   st.sucess("**UPLOAD DE DADOS**")
 
-  dados = st.file_uploader "Carregue...",
+  dados = st.file_uploader("Carregue...",
       type=["xlsx", "xls"]
 )
 if dados:
