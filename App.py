6d206cf062bb696ebd259b527288aaa71f71d048
bbmlino-prog <bbmlino@gmail.com>
2025-12-15 11:09:15 -0100

    Update App.py

--- App.py
+++ App.py
@@ -3,7 +3,6 @@ from streamlit_option_menu import option_menu
 import numpy as np
 import pandas as pd
 
-
 st.header("Introduzindo os elementos do Streamlit") 
 
 menu = option_menu(menu_title="Menu",
